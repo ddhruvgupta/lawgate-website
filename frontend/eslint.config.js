796aa15056a1a34ce2862b2e355a3af8c12171af
ddhruvgupta <ddhruvgupta@gmail.com>
2025-10-20 21:26:44 -0400

    fix eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
@@ -26,4 +26,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+]
